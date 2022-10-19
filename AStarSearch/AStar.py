7d2e0661a2327b4170d854cc6f58af282f439177
AhmedElemary57 <honor5766@gmail.com>
2022-10-19 17:00:49 +0200

    update costFunction

--- AStarSearch/AStar.py
+++ AStarSearch/AStar.py
@@ -14,21 +14,19 @@ class AStar:
 
     def search(self):
         maxDepth = 0
-        # I have make priority queue of frontier to push state into it based on the cost
-        # that should be equal to g(n) + h(n) cost is calculated from getCost based on heuristicsType
         frontier = []
         frontierSet = {self.initialState.stateSavedAsInt: 0}
         heapify(frontier)
-        heappush(frontier, (AStar.getCost(self, self.initialState), 0,
+        heappush(frontier, (AStar.getCost(self, self.initialState),
+                            0,
+                            0,
                             self.initialState.stateSavedAsInt,
                             self.initialState))
-        # We will loop till frontier is not empty to search for the solution
         while not len(frontier) == 0:
-            state = heappop(frontier)[3]  # We accessed index 2 as in priority queue we have tuple of (cost, stateString, state)
-            self.explored.add(state.stateSavedAsInt)  # Add to the set of visited elements the current state as string
-            if self.goalState.stateSavedAsInt == state.stateSavedAsInt:  # If we found the needed state, so we are done and return the state
+            state = heappop(frontier)[4]
+            self.explored.add(state.stateSavedAsInt)
+            if self.goalState.stateSavedAsInt == state.stateSavedAsInt:
                 return state, maxDepth
-            # case not fount we expand and generate the children of the current state
             neighborPriority = 0
             for neighbor in state.generateChildren():
                 neighborPriority += 1
@@ -37,24 +35,35 @@ class AStar:
                     cost = AStar.getCost(self, neighbor)
                     heappush(frontier, (cost,
                                         neighborPriority,
+                                        neighbor.depth,
                                         neighbor.stateSavedAsInt,
                                         neighbor))
                     frontierSet[neighbor.stateSavedAsInt] = cost
+
                 elif neighbor.stateSavedAsInt not in self.explored:
                     if frontierSet[neighbor.stateSavedAsInt] > AStar.getCost(self, neighbor):
                         heappush(frontier, (AStar.getCost(self, neighbor),
                                             neighborPriority,
+                                            neighbor.depth,
                                             neighbor.stateSavedAsInt,
                                             neighbor))
                         frontierSet[neighbor.stateSavedAsInt] = AStar.getCost(self, neighbor)
         return None, maxDepth
+
+    def addStateToFrontier(self, frontier,frontierSet , state):
+        cost = AStar.getCost(self, state)
+        heappush(frontier, (cost,
+                            state,
+                            state.depth,
+                            state.stateSavedAsInt,
+                            state))
+        frontierSet[state.stateSavedAsInt] = cost
 
     def printPath(self, state):
         if state.parent is not None:
             self.printPath(state.parent)
         state.printState()
 
-    # we will assume that the cost of one move will be 1
     def getCost(self, state):
         stateAsString = state.stateAsString()
         goalState = self.goalState.stateAsString()
@@ -95,3 +104,7 @@ class AStar:
         print(f"Search Max Depth is : {report[3]}\n ")
         print(f"Running Time is : {report[4]}")
 
+
+astar = AStar(State(123456780), State(12345678), "manhattan")
+report = astar.getReport()
+print(report)
--- AStarSearch/AStarTest.py
+++ AStarSearch/AStarTest.py
@@ -18,22 +18,14 @@ class MyTestCase(unittest.TestCase):
     def testSolvePuzzleWithDepth27Manhattan(self):
         astar = AStar(State(54861732), State(12345678), "manhattan")
         report = astar.getReport()
-        self.assertEqual(report[0],
-                         ['right', 'right', 'down', 'left', 'left', 'down', 'right', 'up', 'right', 'down', 'left',
-                          'up', 'left', 'down', 'right', 'up', 'up', 'left', 'down', 'right', 'up', 'right', 'down',
-                          'left', 'left', 'up'])
+        self.assertEqual(report[1], 26)
 
     def testSolvePuzzleWithDepth27Euclidian(self):
         astar = AStar(State(54861732), State(12345678), "euclidian")
         report = astar.getReport()
-        self.assertEqual(report[0],
-                         ['right', 'down', 'left', 'down', 'right', 'up', 'right', 'down', 'left', 'left', 'up', 'up',
-                          'right', 'down', 'left', 'up', 'right', 'right', 'down', 'left', 'down', 'left', 'up',
-                          'right', 'up', 'left'])
+        self.assertEqual(report[1], 26)
 
-    # this state is made to dimostrant that a* is better in case of manhattan than euclidian
-    # because in bid test cases manhattan hs less expand nodes than euclidian
-    def testcompareExpandOfPuzzleWithDepth27ManhattanAndEuclidian(self):
+    def testCompareExpandOfPuzzleWithDepth27ManhattanAndEuclidian(self):
         astarM = AStar(State(54861732), State(12345678), "manhattan")
         reportM = astarM.getReport()
         astarD = AStar(State(54861732), State(12345678), "euclidian")
@@ -45,20 +37,14 @@ class MyTestCase(unittest.TestCase):
         goalState = State(123456780)
         astar = AStar(initialState, goalState, "manhattan")
         report = astar.getReport()
-        self.assertEqual(report[0],
-                         ['right', 'up', 'up', 'left', 'left', 'down', 'down', 'right', 'right', 'up', 'left', 'left',
-                          'down', 'right', 'right', 'up', 'left', 'up', 'right', 'down', 'left', 'up', 'left', 'down',
-                          'down', 'right', 'right', 'up', 'left', 'down', 'right'])
+        self.assertEqual(report[1],31)
 
-    def testComplexProblemeEuclidian(self):
+    def testComplexProblemEuclidian(self):
         initialState = State(867254301)
         goalState = State(123456780)
         astar = AStar(initialState, goalState, "euclidian")
         report = astar.getReport()
-        self.assertEqual(report[0],
-                         ['left', 'up', 'right', 'up', 'right', 'down', 'down', 'left', 'up', 'up', 'right', 'down',
-                          'down', 'left', 'up', 'left', 'up', 'right', 'right', 'down', 'left', 'left', 'down', 'right',
-                          'up', 'left', 'up', 'right', 'down', 'right', 'down'])
+        self.assertEqual(report[1], 31)
 
     def testUnsolvableManhattan(self):
         initialState = State(812043765)
