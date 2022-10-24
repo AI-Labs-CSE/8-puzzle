79e7310b54d9bab8eb0b70f2a27b6116eeff7915
Ahmedelsa3eed <73740339+Ahmedelsa3eed@users.noreply.github.com>
2022-10-24 23:22:24 +0200

    refactoring UI

--- Visualizer.py
+++ Visualizer.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from Utilities import get_coordinates_of
 
+
 index = -1
 stop = True
 labels = []
@@ -8,16 +9,49 @@ position = []
 path_to_goal = []
 root = None
 
-# Change the position of the zero with the given values, also change the colors between red and black
-def change_position(row_change, col_change):
-    global labels, position
-    labels[position[0]][position[1]]['text'] = labels[position[0] + row_change][position[1] + col_change]['text']
-    labels[position[0]][position[1]]['fg'] = '#000000'
-    labels[position[0] + row_change][position[1] + col_change]['text'] = '0'
-    labels[position[0] + row_change][position[1] + col_change]['fg'] = '#FF0000'
-    position[0] += row_change
-    position[1] += col_change
-    return position
+# Draw the initial state and make every move in the given array path_to_goal
+def visualize(initial_state, moves):
+    global labels, position, path_to_goal, root
+    path_to_goal = moves
+    
+    root = create_root_window()
+    
+    labels = create_board_labels(initial_state)
+
+    # Get the coordinates of 0 in the initial state to keep track of its movement and change its color to red
+    position = get_coordinates_of("0", initial_state)
+    labels[position[0]][position[1]]['fg'] = '#FF0000'
+    
+    Button(root, text = "Play", command = play).grid(row = 3, column = 0,  sticky = NSEW)
+    Button(root, text = "Pause", command = pause).grid(row = 3, column = 2,  sticky = NSEW)
+    Button(root, text = "Back", command = back).grid(row = 4, column = 0,  sticky = NSEW)
+    Button(root, text = "Next", command = next).grid(row = 4, column = 2,  sticky = NSEW)
+    
+    root.after(1000, update)
+    
+    root.mainloop()
+    
+def create_root_window():
+    """ Make a fixed sized window
+    """
+    root = Tk()
+    root.geometry("400x400")
+    root.columnconfigure(tuple(range(3)), weight = 1)
+    root.rowconfigure(tuple(range(5)), weight = 1)
+    return root
+
+def create_board_labels(initial_state):
+    """ Represent the labels in 2d array as a 3 * 3 grid 
+        with big bold font size, initialized with the initial state
+    """
+    labels = []
+    for i in range(0, 3):
+        temp = []
+        for j in range(0, 3):
+            temp.append(Label(master = root, text = initial_state[i * 3 + j], font = ("Helvetica", 36, "bold")))
+            temp[j].grid(row = i, column = j, sticky = NSEW)
+        labels.append(temp)
+    return labels
 
 def update():
     global index, stop, path_to_goal, root
@@ -39,7 +73,20 @@ def update():
             change_position(0, 1)
     
     root.after(1000, update)
-        
+
+def change_position(row_change, col_change):
+    """ Change the position of the zero with the given values,
+        also change the colors between red and black
+    """
+    global labels, position
+    labels[position[0]][position[1]]['text'] = labels[position[0] + row_change][position[1] + col_change]['text']
+    labels[position[0]][position[1]]['fg'] = '#000000'
+    labels[position[0] + row_change][position[1] + col_change]['text'] = '0'
+    labels[position[0] + row_change][position[1] + col_change]['fg'] = '#FF0000'
+    position[0] += row_change
+    position[1] += col_change
+    return position
+
 def play():
     global stop
     stop = False
@@ -53,7 +100,7 @@ def back():
     stop = True
     if index < 0:
         return
-    index -= 1        
+    index -= 1    
     if path_to_goal[index + 1] == 'Up':
         change_position(1, 0)
     elif path_to_goal[index + 1] == 'Down':
@@ -80,35 +127,3 @@ def next():
     elif path_to_goal[index] == 'Right':
         change_position(0, 1)
 
-# Draw the initial state and make every move in the given array path_to_goal
-def visualize(initial_state, moves):
-    global labels, position, path_to_goal, root
-    path_to_goal = moves
-    # Make a fixed size window
-    root = Tk()
-    root.geometry("400x400")
-    root.columnconfigure(tuple(range(3)), weight = 1)
-    root.rowconfigure(tuple(range(5)), weight = 1)
-    
-    # Represent the labels in 2d array as a 3 * 3 grid with big bold font size, initialized with the initial state
-    labels = []
-    for i in range(0, 3):
-        temp = []
-        for j in range(0, 3):
-            temp.append(Label(master = root, text = initial_state[i * 3 + j], font = ("Helvetica", 36, "bold")))
-            temp[j].grid(row = i, column = j, sticky = NSEW)
-        labels.append(temp)
-
-    # Get the coordinates of 0 in the initial state to keep track of its movement and change its color to red
-    position = get_coordinates_of("0", initial_state)
-    labels[position[0]][position[1]]['fg'] = '#FF0000'
-    
-    Button(root, text = "Play", command = play).grid(row = 3, column = 0,  sticky = NSEW)
-    Button(root, text = "Pause", command = pause).grid(row = 3, column = 2,  sticky = NSEW)
-    Button(root, text = "Back", command = back).grid(row = 4, column = 0,  sticky = NSEW)
-    Button(root, text = "Next", command = next).grid(row = 4, column = 2,  sticky = NSEW)
-    
-    root.after(1000, update)
-    
-    root.mainloop()
-    
